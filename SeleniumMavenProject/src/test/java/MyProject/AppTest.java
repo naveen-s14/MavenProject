15bca69040b42d520b2667b6f1b20ee95bc29621
naveen-s14 <nivn100@gmail.com>
2020-08-05 11:35:36 +0530

    first developer

---
+++ SeleniumMavenProject/src/test/java/MyProject/AppTest.java
@@ -0,0 +1,32 @@
+package MyProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+
+    public void FirstDeveloper() {
+    	assertTrue( true );
+    }
+
+    public void HelloTesting()
+//github.com/naveen-s14/MavenProject.git
+    {
+        assertTrue( true );
+    }
+}
